fbf6e1dd199803fce084f5389a2ae5d32b4e311d
ibra-exe <ibrahim_x@msn.com>
2023-11-16 23:32:47 +0300

    first push

---
+++ typewriter.js
@@ -0,0 +1,25 @@
+const typeTarget = document.getElementById("type-target");
+const fullText = "Hi, I'm Ibrahim Shaheen, Ibra for short.      a Software Engineer by day and an Alien by... well, always – because who else but an extraterrestrial could understand this Earthly code mess?";
+typeTarget.textContent = "|";
+let charIndex = 0;
+
+function typeChar() {
+    if (charIndex < fullText.length) {
+        typeTarget.textContent = fullText.slice(0, charIndex + 1) + "|";
+        charIndex++;
+        setTimeout(typeChar, 50);  // speed of typing, adjust as needed
+    } else {
+        toggleCursorBlinking();
+    }
+}
+
+function toggleCursorBlinking() {
+    if(typeTarget.textContent.endsWith("_")) {
+        typeTarget.textContent = fullText;
+    } else {
+        typeTarget.textContent = fullText + "_";
+    }
+    setTimeout(toggleCursorBlinking, 500);
+}
+
+typeChar();
