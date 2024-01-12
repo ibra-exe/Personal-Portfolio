2d9fdea7c5f46e822d46b44f4ebe8ecabdeb30f5
ibra-exe <ibrahim_x@msn.com>
2024-01-12 20:52:22 +0300

    about section completed

--- about-typewriter.js
+++ about-typewriter.js
@@ -1,5 +1,5 @@
 const aboutTypeTarget = document.getElementById("about-type-target");
-const aboutFullText = "May 16th, 1993.\nIbrahim Shaheen aka Ibra\nSoftware Engineer | Technology Enthusiast\nInterested in:\nTechnology | AI | Automations | Digitalization\nBased in NEOM";
+const aboutFullText = "May 16th, 1993.\n\nIbrahim Shaheen aka Ibra\n\nSoftware Engineer | Technology Enthusiast\n\nInterested in:\n\nTechnology | AI | Automations | Digitalization\n\nBased in NEOM";
 let aboutCharIndex = 0;
 
 function aboutTypeChar() {
@@ -7,8 +7,25 @@ function aboutTypeChar() {
         aboutTypeTarget.textContent = aboutFullText.slice(0, aboutCharIndex + 1) + "|";
         aboutCharIndex++;
         setTimeout(aboutTypeChar, 50);  // speed of typing, adjust as needed
+    } else {
+        // Remove the vertical line cursor at the end and start blinking underscore
+        aboutTypeTarget.textContent = aboutFullText; // Remove the "|" at the end
+        toggleCursorBlinking();
     }
 }
 
+function toggleCursorBlinking() {
+    if (aboutTypeTarget.textContent.endsWith("_")) {
+        aboutTypeTarget.textContent = aboutFullText;
+    } else {
+        aboutTypeTarget.textContent = aboutFullText + "_";
+    }
+    setTimeout(toggleCursorBlinking, 500);
+}
+
+window.onload = () => {
+    document.getElementById('profile-pic').style.opacity = 1;
+};
+
 // Start typing after the image has faded in
-setTimeout(aboutTypeChar, 1000); // Adjust the delay as needed
+setTimeout(aboutTypeChar, 500); // Adjust the delay as needed
